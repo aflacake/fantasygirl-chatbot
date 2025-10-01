23d9c693838a6154d52aa114c9b6373d29bf620e
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-10-01 12:50:19 +0700

    Membuat ui.js
    Fungsi kecil (_helper_).

---
+++ js/ui.js
@@ -0,0 +1,32 @@
+// js/ui.js
+
+export function inisiasiUI() {
+  // bisa nanti isi animasi masuk, dsb.
+}
+
+export function perlihatkanPesanPengguna(teks) {
+  const boksPesan = document.getElementById("pesan");
+  const psn = document.createElement("div");
+  psn.className = "pesan pengguna";
+  psn.innerText = teks;
+  boksPesan.appendChild(psn);
+  boksPesan.scrollTop = boksPesan.scrollHeight;
+}
+
+export function perlihatkanPesanBot(teks) {
+  const boksPesan = document.getElementById("pesan");
+  const psn = document.createElement("div");
+  psn.className = "pesan bot";
+  psn.innerText = teks;
+  boksPesan.appendChild(psn);
+  boksPesan.scrollTop = boksPesan.scrollHeight;
+}
+
+export function setBerbicaraKarakter(berbicara) {
+  const gmbr = document.getElementById("gmbr-karak");
+  if (berbicara) {
+    gmbr.src = "https://raw.githubusercontent.com/aflacake/fantasygirl-chatbot/main/assets/20250930_181044.png";
+  } else {
+    gmbr.src = "https://raw.githubusercontent.com/aflacake/fantasygirl-chatbot/main/assets/20250930_181055.png";
+  }
+}
